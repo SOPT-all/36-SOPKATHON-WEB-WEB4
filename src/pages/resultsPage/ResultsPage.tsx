9af1c5b4fe3ec0a1df0e597e9ffe52f46c33258a
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-05-18 06:52:43 +0900

    [FEAT] 결과 페이지에 모달 컴포넌트 추가

--- src/pages/resultsPage/ResultsPage.tsx
+++ src/pages/resultsPage/ResultsPage.tsx
@@ -1,21 +1,38 @@
-import { css } from '@emotion/react';
-import type { ReactNode } from 'react';
+/** @jsxImportSource @emotion/react */
+import { useState } from 'react';
 import * as S from './resultsPage.style';
-
-interface ResultsPagePropTypes {
-  children?: ReactNode;
-}
+import Button from '@components/button/Button';
+import PartnerModal from '@components/modal/PartnerModal';
 
 const ResultsPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const partnerNames = ['ㅇㅇㅇ', 'ㅇㅇㅇ', 'ㅇㅇㅇ', 'ㅇㅇㅇ', 'ㅇㅇㅇ', 'ㅇㅇㅇ','ㅇㅇㅇ', 'ㅇㅇㅇ', 'ㅇㅇㅇ', 'ㅇㅇㅇ', 'ㅇㅇㅇ', 'ㅇㅇㅇ', 'ㅇㅇㅇ', 'ㅇㅇㅇ', 'ㅇㅇㅇ'];
+
   return (
     <div css={S.Wrapper}>
-    <p css={S.title}>OOOO님의 마실지도는...</p>
-    <div css={S.container}></div>
-    <p css={S.location}>창원시</p>
-    <p css={S.drink}>맑은 내일 벚꽃주</p>
-    <p css={S.content}>봄이 되면 떠오르는 벚꽃의 색감과 향을 담은 달콤한 술, 청주 스타일, 진해군항제 기념주</p>
+      <p css={S.title}>OOOO님의 마실지도는...</p>
+      <div css={S.container}></div>
+      <p css={S.location}>창원시</p>
+      <p css={S.drink}>맑은 내일 벚꽃주</p>
+      <p css={S.content}>
+        봄이 되면 떠오르는 벚꽃의 색감과 향을 담은 달콤한 술, 청주 스타일, 진해군항제 기념주
+      </p>
+
+      <div css={S.buttonWrapper}>
+        <Button
+          text="이름 확인하기"
+          isEnabled={true}
+          onClick={() => setIsModalOpen(true)}
+        />
+      </div>
+      <PartnerModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        names={partnerNames}
+      />
     </div>
   );
 };
 
-export default ResultsPage; 
+export default ResultsPage;
