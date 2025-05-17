a5617690f56261f4f46d4636195b09cee139e15a
Kyoungho Eom <kheom0512@gmail.com>
2025-05-18 08:52:05 +0900

    23532

--- src/apis/apiClient.ts
+++ src/apis/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://ec2-54-180-116-83.ap-northeast-2.compute.amazonaws.com',
+  baseURL: 'https://54.180.116.83.nip.io',
   headers: {
     'Content-Type': 'application/json',
   },
