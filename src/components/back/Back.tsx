da80a6edd4dc02f0dc20a216b66198240a40b0e1
earl9rey <earl9rey@naver.com>
2025-05-18 05:17:39 +0900

    [Feat] 뒤로가기 구현

--- src/components/back/Back.tsx
+++ src/components/back/Back.tsx
@@ -1,11 +1,14 @@
 /** @jsxImportSource @emotion/react */
+import { useNavigate } from 'react-router-dom';
 import { backStyle } from './back.style';
 import { BackIcon } from '@assets/svgs';
 
 const Back = () => {
+    const navigate = useNavigate();
+
   return (
     <div css={backStyle}>
-        <BackIcon/>
+        <BackIcon onClick={() => navigate(-1)}/>
     </div>
   );
 };
