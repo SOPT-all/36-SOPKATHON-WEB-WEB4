18d0bf5fbe885dec91aa2bb0cb4ec847ae289838
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-05-18 03:21:05 +0900

    [STYLE] 스텝 카운터 컴포넌트 theme.ts 스타일 적용

--- src/components/stepCounter/StepCounter.style.ts
+++ src/components/stepCounter/StepCounter.style.ts
@@ -1,12 +1,9 @@
 import { css } from '@emotion/react';
-import theme from '@styles/theme';
+import theme from '@/styles/theme';
 
 export const stepCounterStyle = css`
-    color: #000;
-    text-align: center;
-    font-family: "Pretendard Variable";
-    font-size: 1.4rem;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
+  color: ${theme.colors['text-sub1']};
+  ${theme.fonts['body2-sb-14']};
+  width: 12rem;
+  text-align: center;
 `;
