a2288f14438e69431bfbf000a741134bb8b5a1ed
Kyoungho Eom <kheom0512@gmail.com>
2025-05-18 01:08:44 +0900

    feat: 라우터 도입 및 질문 페이지 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { Global, ThemeProvider } from '@emotion/react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import QuestionPage from '@pages/questionPage/QuestionPage';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyle} />
+        <Routes>
+          <Route path="/" element={<div>홈 페이지</div>} />
+          <Route path="/question" element={<QuestionPage />} />
+        </Routes>
       </ThemeProvider>
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/pages/questionPage/QuestionPage.tsx
+++ src/pages/questionPage/QuestionPage.tsx
@@ -10,7 +10,7 @@ const QuestionPage = ({ children }: QuestionPagePropTypes) => {
   return (
     <div css={S.Wrapper}>
       <h1>
-        test
+        질문 페이지입니다.
       </h1>
       {children}
     </div>
