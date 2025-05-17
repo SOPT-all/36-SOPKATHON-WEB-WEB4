602e35b90b84925d29875f13dcdb8abf8a20f6a4
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-05-18 02:26:03 +0900

    [FEAT] 스텝 카운터 컴포넌트 구현

---
+++ src/components/stepCounter/StepCounter.style.ts
@@ -0,0 +1,12 @@
+import { css } from '@emotion/react';
+import theme from '@styles/theme';
+
+export const stepCounterStyle = css`
+    color: #000;
+    text-align: center;
+    font-family: "Pretendard Variable";
+    font-size: 1.4rem;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+`;
