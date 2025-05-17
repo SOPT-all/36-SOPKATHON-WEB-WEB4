a798a5872097d6fba168c8b3893bf22b88ba999b
heeyyeon <wjdgmldus28@gachon.ac.kr>
2025-05-18 05:08:50 +0900

    [FEAT] 질문 페이지 api 연동

--- src/pages/questionPage/QuestionPage.tsx
+++ src/pages/questionPage/QuestionPage.tsx
@@ -1,21 +1,40 @@
-import { BackIcon } from '@assets/svgs'
-import ButtonGroup from './buttonGroup/buttonGroup'
-import * as styles from "./questionPage.style"
+/** @jsxImportSource @emotion/react */
+import { useEffect, useState } from 'react';
+import { BackIcon } from '@assets/svgs';
+import ButtonGroup from '@pages/questionPage/buttonGroup/buttonGroup';
+import * as styles from './questionPage.style';
+import { getQuestions } from '@/apis/questions';
+import { Question } from '@/types/question';
 
 const QuestionPage = () => {
+  const [question, setQuestion] = useState<Question | null>(null);
 
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const data = await getQuestions();
+        setQuestion(data[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetch();
+  }, []);
 
   return (
     <div css={styles.testWrapper}>
       <BackIcon css={styles.icon} />
       <div css={styles.questionContainer}>
-        <img src="/logo.png" alt="테스트 이미지" css={styles.testImg}/>
-        <h1 css={styles.testText}>가장 끌리는 풍경이 있나요?</h1>
+       <img
+          src={question?.imageUrl ?? ''}
+          alt="질문 이미지"
+          css={styles.testImg}
+        />
+        <h1 css={styles.testText}>{question?.title ?? ''}</h1>
       </div>
       <ButtonGroup />
     </div>
-  )
-}
+  );
+};
 
-export default QuestionPage
-
+export default QuestionPage;
