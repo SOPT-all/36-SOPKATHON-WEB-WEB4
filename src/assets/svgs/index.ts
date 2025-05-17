b94fecbb2106c2912782830cf4904e97c2a876d3
Kyoungho Eom <kheom0512@gmail.com>
2025-05-18 04:16:37 +0900

    feat: 로고 텍스트 SVG 아이콘 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,2 +1,4 @@
-export { default as XIcon } from './XIcon';
 export { default as BackIcon } from './BackIcon';
+export { default as LogoTextMain } from './LogoTextMain';
+export { default as LogoTextWhite } from './LogoTextWhite';
+export { default as XIcon } from './XIcon';
