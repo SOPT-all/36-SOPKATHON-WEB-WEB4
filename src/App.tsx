6c685b2961ca52ff7a4f9c4b0b634ed2af9eb9c0
earl9rey <earl9rey@naver.com>
2025-05-18 02:23:41 +0900

    [CHORE] Result 페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Global, ThemeProvider } from '@emotion/react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import QuestionPage from '@pages/questionPage/QuestionPage';
+import ResultsPage from '@pages/resultsPage/ResultsPage';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<div>홈 페이지</div>} />
           <Route path="/question" element={<QuestionPage />} />
+          <Route path="/result" element={<ResultsPage />} />
         </Routes>
       </ThemeProvider>
     </BrowserRouter>
